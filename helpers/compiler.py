28941d46f31bd0551eddc2cbf025a0c9b6ad476d
Farid Ahmadian <farid.ahmadian@bonial.com>
2020-12-28 19:20:20 +0100

    Refactor the compiler class

--- helpers/compiler.py
+++ helpers/compiler.py
@@ -12,31 +12,45 @@ logger = Logger.initial(__name__)
 
 
 class Compiler:
-    """Will download MarkDown file and generate HTML from downloaded MDs"""
+    """Will download MarkDown file and generate static website from downloaded MDs if it is needed"""
 
     @staticmethod
-    def check_website_and_save_new_contents(url: str, category: str, label: str, now: str):
-
+    def _generate_md_file_address(url: str, markdown_directory_path: str) -> str:
         file_name_hash = hashlib.md5(url.encode("utf-8")).hexdigest()
-        markdown_directory_path = f"website/docs/{category}/"
         markdown_file_path = markdown_directory_path + file_name_hash + ".md"
-        logger.info(f"Check Url: {url}")
+        return markdown_file_path
 
+    @staticmethod
+    def _get_website_content(url: str) -> tuple:
+        logger.info(f"Check Url: {url}")
         html = UrlOpener.open(url)
         url_content_hash = hashlib.md5(html.encode("utf-8")).hexdigest()
         logger.info(url_content_hash)
+        return url_content_hash, html
+
+    @staticmethod
+    def _write_into_file(file: str, filecontent: str, writemode: str):
+        with open(file, writemode) as file:
+            if writemode == "wb":
+                file.write(filecontent.encode("utf-8"))
+            else:
+                file.write(filecontent)
+            logger.info(f"File was writen in {file}")
+
+    @staticmethod
+    def check_website_and_save_new_contents(url: str, category: str, label: str, now: str):
+        markdown_directory_path = f"website/docs/{category}/"
+        markdown_file_path = Compiler._generate_md_file_address(url, markdown_directory_path)
+
+        url_content_hash, url_content_html = Compiler._get_website_content(url)
 
         # If something detect as a changed case from DB side we should save MD file to the disk and trigger HTML creator
         if DB.insert_only_new_content(url, markdown_file_path, url_content_hash, category, label, now):
             Path(markdown_directory_path).mkdir(parents=True, exist_ok=True)
-
-            with open(markdown_file_path, "wb") as file:
-                file.write(html.encode("utf-8"))
-                logger.info(f"File was writen in {markdown_file_path}")
+            Compiler._write_into_file(markdown_file_path, url_content_html, "wb")
 
     @staticmethod
-    def generate_new_menu():
-
+    def _get_menu_items_from_db() -> dict:
         menu_items = {}
         db_menu_items = DB.get_markdowns_menu()
         for item in db_menu_items:
@@ -47,16 +61,19 @@ class Compiler:
         menu["nav"].append({"Home": "index.md"})
         for item in menu_items:
             menu["nav"].append({item: menu_items[item]})
-        logger.info(f" Menu items: {menu}")
+        return menu
 
-        with open("website/mkdocs.yml", 'w') as stream:
-            stream.write(yaml.dump(menu))
+    @staticmethod
+    def _generate_new_menu():
+        menu = Compiler._get_menu_items_from_db()
+        logger.info(f" Menu items: {menu}")
+        Compiler._write_into_file("website/mkdocs.yml", yaml.dump(menu), 'w')
 
     @staticmethod
     def generate_new_static_html_site(now: str):
 
         if DB.new_update(now):
-            Compiler.generate_new_menu()
+            Compiler._generate_new_menu()
 
             p1 = subprocess.run(['mkdocs', 'build', '--clean'], capture_output=True, text=True, cwd='website')
             logger.info(p1.stdout)
