a0c772edd40ef5e8329fbe429570ead78f5dc2bd
Nils Mull <nils.mull@bonial.com>
2021-10-13 12:11:37 +0200

    refactor: Adds exception if no provider for an url can be found

--- helpers/providers/urlopener.py
+++ helpers/providers/urlopener.py
@@ -15,13 +15,28 @@ PROVIDERS = [
 ]
 
 
+class NoProviderFoundException(Exception):
+
+    def __init__(self, url, message):
+        self.url = url
+        self.message = message
+        super().__init__(self.message)
+
+
 class UrlOpener:
     """Handle authentication automatically if it's needed and get the content"""
 
     @staticmethod
     def open(desired_url: str, url_type: str):
         logger.info(f"Check Url: {desired_url}")
-        for provider in PROVIDERS:
-            if provider.can_open(desired_url):
-                logger.debug(f"Using {provider} as provider for {desired_url}")
-                return provider.get_page(desired_url)
+
+        capable_provider = [
+            provider for provider in PROVIDERS if provider.can_open(desired_url)]
+        if [] == capable_provider:
+            msg = f"No capable provider found for {desired_url}"
+            logger.error(msg)
+            raise NoProviderFoundException(desired_url, msg)
+
+        provider = capable_provider[0]  # Take first capable provider
+        logger.debug(f"Using {provider} as provider for {desired_url}")
+        return provider.get_page(desired_url)
