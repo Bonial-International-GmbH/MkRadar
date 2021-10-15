fc64c87edc4e58622fc3163b815873d69a5a4fea
Nils Mull <nils.mull@bonial.com>
2021-10-15 10:29:14 +0200

    refactor(compiler): Linting and comments

--- helpers/compiler.py
+++ helpers/compiler.py
@@ -1,15 +1,16 @@
 """Save markdowns to the disks from the web"""
 import hashlib
-import yaml
 import subprocess
+from os.path import basename, join
 from pathlib import Path
-from helpers.providers import UrlOpener
+
+import yaml
+from helpers.aws_helper import AWS
+from helpers.cleaner import Cleaner
+from helpers.config_handler import ConfigHandler
 from helpers.db_handler import DB
 from helpers.logger import Logger
-from helpers.cleaner import Cleaner
-from helpers.aws_helper import AWS
-from helpers.config_handler import ConfigHandler
-from os.path import join, basename
+from helpers.providers import UrlOpener
 
 logger = Logger.initial(__name__)
 
@@ -19,15 +20,17 @@ class Compiler:
 
     @staticmethod
     def get_project_root() -> Path:
+        ''' Retrieves base directory '''
         return Path(__file__).parent.parent
 
-
     @staticmethod
-    def _get_all_mds_address_from_config_file(website_path: str) -> list:
+    def _get_all_mds_address_from_config_file(website_path: str) -> list[str]:
+        ''' Returns a list of the hashes of the markdown files '''
         return [Compiler._generate_md_file_address(md["url"], md["category"], website_path) for md in ConfigHandler.get_mk_pages()]
 
     @staticmethod
     def _generate_md_file_address(url: str, category: str, website_path: str) -> str:
+        ''' Create directory for the markdown file and returns hashed filenames '''
         markdown_directory_path = join(website_path, "docs", category)
         Path(markdown_directory_path).mkdir(parents=True, exist_ok=True)
         file_name = hashlib.md5(url.encode("utf-8")).hexdigest() + ".md"
@@ -35,8 +38,9 @@ class Compiler:
         return markdown_file_path
 
     @staticmethod
-    def _get_website_content(url: str, url_type: str) -> tuple:
-        logger.info(f"Check Url: {url}")
+    def _get_website_content(url: str, url_type: str) -> tuple[str, str]:
+        ''' Returns downloaded content and hash of the content '''
+        logger.info("Check Url: %s", url)
         html = UrlOpener.open(url, url_type)
         url_content_hash = hashlib.md5(html.encode("utf-8")).hexdigest()
         logger.info(url_content_hash)
@@ -44,27 +48,32 @@ class Compiler:
 
     @staticmethod
     def _write_into_file(file: str, filecontent: str, writemode: str):
-        with open(file, writemode) as file:
+        ''' writes content into file '''
+        with open(file, writemode) as output_file:
             if writemode == "wb":
-                file.write(filecontent.encode("utf-8"))
+                output_file.write(filecontent.encode("utf-8"))
             else:
-                file.write(filecontent)
-            logger.info(f"File was writen in {file}")
+                output_file.write(filecontent)
+            logger.info("File was writen in %s", file)
 
     @staticmethod
     def save_content_if_it_was_new(url: str, category: str, title: str, now: str, website_path: str, url_type: str = "public"):
+        ''' Check content against database and if new writes new file '''
+        markdown_file_path = Compiler._generate_md_file_address(
+            url, category, website_path)
 
-        markdown_file_path = Compiler._generate_md_file_address(url, category, website_path)
-
-        url_content_hash, url_content_html = Compiler._get_website_content(url, url_type)
+        url_content_hash, url_content_html = Compiler._get_website_content(
+            url, url_type)
 
         # If something detect as a changed case from DB side we should save MD file to the disk and trigger HTML creator
         if DB.insert_only_new_content(url, markdown_file_path, url_content_hash, category, title, now):
-            Compiler._write_into_file(markdown_file_path, url_content_html, "wb")
+            Compiler._write_into_file(
+                markdown_file_path, url_content_html, "wb")
 
     @staticmethod
     def _get_menu_items_from_db() -> dict:
-        menu_items = {}
+        ''' Return a dict with all mardown items in database '''
+        menu_items: dict = {}
         db_menu_items = DB.get_markdowns_menu()
         for item in db_menu_items:
             title, file_address, category = item[0], basename(item[1]), item[2]
@@ -73,28 +82,29 @@ class Compiler:
         menu_items = dict(sorted(menu_items.items()))
         return menu_items
 
-
     @staticmethod
     def _generate_new_mkdocs_config(website_path: str):
         menu_items = Compiler._get_menu_items_from_db()
-        logger.info(f" Menu items: {menu_items}")
+        logger.info(" Menu items: %s", menu_items)
         mkdocs_config = {
             "site_name": "MkRadar",
             "site_dir": "html",
             "use_directory_urls": False,
-            "theme":  {
-                "name": "material"} ,
+            "theme": {
+                "name": "material"},
             "nav": []}
         mkdocs_config["nav"].append({"Home": "index.md"})
         for item in menu_items:
             mkdocs_config["nav"].append({item: menu_items[item]})
 
-        Compiler._write_into_file(join(website_path, "mkdocs.yml"), yaml.dump(mkdocs_config), 'w')
+        Compiler._write_into_file(
+            join(website_path, "mkdocs.yml"), yaml.dump(mkdocs_config), 'w')
 
     @staticmethod
     def _copy_index_md_to_docs(website_path: str):
         with open(join(Compiler.get_project_root(), 'index.md')) as file:
-            Compiler._write_into_file(join(website_path, "docs", "index.md"), file.read(), 'w')
+            Compiler._write_into_file(
+                join(website_path, "docs", "index.md"), file.read(), 'w')
 
     @staticmethod
     def generate_new_static_html_site_if_it_is_needed(
@@ -108,10 +118,11 @@ class Compiler:
         if config_file_change_detected or any_new_item_in_the_db:
             Compiler._generate_new_mkdocs_config(website_path)
             Compiler._copy_index_md_to_docs(website_path)
-            p1 = subprocess.run(['mkdocs', 'build', '--clean'], capture_output=True, text=True, cwd=website_path)
-            logger.info(p1.stdout)
-            logger.error(p1.stderr)
+            mkdocs_process = subprocess.run(['mkdocs', 'build', '--clean'],
+                                            capture_output=True, text=True, cwd=website_path)
+            logger.info(mkdocs_process.stdout)
+            logger.error(mkdocs_process.stderr)
             if s3_bucket_name:
                 AWS.clean_s3_bucket(s3_bucket_name)
-                AWS.copy_to_s3(website_path, s3_bucket_name, s3_bucket_destination)
-
+                AWS.copy_to_s3(website_path, s3_bucket_name,
+                               s3_bucket_destination)
